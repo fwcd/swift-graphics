2515a663d78d7f7e36eb2fd600adf13854801deb
fwcd <fwcdmail@gmail.com>
2020-09-29 22:34:35 +0200

    Add dependencies

--- Package.swift
+++ Package.swift
@@ -14,14 +14,18 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0")
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
+        .package(url: "https://github.com/fwcd/swift-utils.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Graphics",
-            dependencies: []
+            dependencies: [
+                .product(name: "Logging", package: "swift-log"),
+                .product(name: "Utils", package: "swift-utils")
+            ]
         ),
         .testTarget(
             name: "GraphicsTests",
