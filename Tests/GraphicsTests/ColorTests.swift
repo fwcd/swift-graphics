2f68a4a2f7465d649325cc1020c14eb498dd806d
fwcd <fwcdmail@gmail.com>
2020-09-29 22:46:41 +0200

    Add unit tests

---
+++ Tests/GraphicsTests/ColorTests.swift
@@ -0,0 +1,33 @@
+import XCTest
+@testable import Graphics
+
+final class ColorTests: XCTestCase {
+	static var allTests = [
+		("testColor", testColor)
+	]
+
+	func testColor() throws {
+		let yellowRGB = Color(rgb: 0xFFFF00)
+		XCTAssertEqual(yellowRGB.red, 0xFF)
+		XCTAssertEqual(yellowRGB.green, 0xFF)
+		XCTAssertEqual(yellowRGB.blue, 0)
+		XCTAssertEqual(yellowRGB.alpha, 0xFF)
+		XCTAssertEqual(yellowRGB.rgb, 0xFFFF00)
+
+		let transparentCyanRGBA = Color(rgba: 0x00FFFFCC)
+		XCTAssertEqual(transparentCyanRGBA.red, 0)
+		XCTAssertEqual(transparentCyanRGBA.green, 0xFF)
+		XCTAssertEqual(transparentCyanRGBA.blue, 0xFF)
+		XCTAssertEqual(transparentCyanRGBA.alpha, 0xCC)
+		XCTAssertEqual(transparentCyanRGBA.rgba, 0x00FFFFCC)
+		XCTAssertEqual(transparentCyanRGBA.argb, 0xCC00FFFF)
+		XCTAssertEqual(transparentCyanRGBA.rgb, 0x00FFFF)
+
+		let magentaARGB = Color(argb: 0xAAFF00FF)
+		XCTAssertEqual(magentaARGB.red, 0xFF)
+		XCTAssertEqual(magentaARGB.green, 0x00)
+		XCTAssertEqual(magentaARGB.blue, 0xFF)
+		XCTAssertEqual(magentaARGB.alpha, 0xAA)
+		XCTAssertEqual(magentaARGB.argb, 0xAAFF00FF)
+	}
+}
