0b2892445d90ae0e9ee0b556edf255f80ba6b293
fwcd <fwcdmail@gmail.com>
2023-01-01 22:48:50 +0100

    Fix coordinates in CoreGraphics backend
    CoreGraphics places its coordinate origin on the bottom left rather than
    the top right, as it seems.

--- Sources/CoreGraphicsGraphics/context/CoreGraphicsContext.swift
+++ Sources/CoreGraphicsGraphics/context/CoreGraphicsContext.swift
@@ -82,6 +82,26 @@ public final class CoreGraphicsContext: GraphicsContext {
         }
     }
 
+    private func cgPoint(for vec2: Vec2<Double>) -> CGPoint {
+        .init(x: vec2.x, y: Double(cgContext.height) - vec2.y)
+    }
+
+    private func cgPoint(for vec2: Vec2<Int>) -> CGPoint {
+        .init(x: vec2.x, y: cgContext.height - vec2.y)
+    }
+
+    private func cgSize(for vec2: Vec2<Double>) -> CGSize {
+        .init(width: vec2.x, height: vec2.y)
+    }
+
+    private func cgSize(for vec2: Vec2<Int>) -> CGSize {
+        .init(width: vec2.x, height: vec2.y)
+    }
+
+    private func cgRect(for rect: Rectangle<Double>) -> CGRect {
+        .init(origin: cgPoint(for: rect.bottomLeft), size: cgSize(for: rect.size))
+    }
+
     private func withPath(color: Color, isFilled: Bool, action: () -> Void) {
         setColor(color, isFilled: isFilled)
         cgContext.beginPath()
@@ -95,25 +115,25 @@ public final class CoreGraphicsContext: GraphicsContext {
 
     public func draw(line: LineSegment<Double>) {
         withPath(color: line.color, isFilled: false) {
-            cgContext.move(to: CGPoint(line.start))
-            cgContext.addLine(to: CGPoint(line.end))
+            cgContext.move(to: cgPoint(for: line.start))
+            cgContext.addLine(to: cgPoint(for: line.end))
         }
     }
 
     public func draw(rect: Rectangle<Double>) {
         withPath(color: rect.color, isFilled: rect.isFilled) {
-            cgContext.addRect(CGRect(rect))
+            cgContext.addRect(cgRect(for: rect))
         }
     }
 
     public func draw(ellipse: Ellipse<Double>) {
         withPath(color: ellipse.color, isFilled: ellipse.isFilled) {
-            cgContext.addEllipse(in: CGRect(ellipse))
+            cgContext.addEllipse(in: cgRect(for: ellipse.boundingRectangle))
         }
     }
 
     public func draw(image: CoreGraphicsImage, at position: Vec2<Double>, withSize size: Vec2<Int>, rotation: Double?) {
-        cgContext.draw(image.cgImage, in: CGRect(origin: CGPoint(position), size: CGSize(size)))
+        cgContext.draw(image.cgImage, in: CGRect(origin: cgPoint(for: position), size: cgSize(for: size)))
     }
 
     public func draw(svg: NoImage, at position: Vec2<Double>, withSize size: Vec2<Int>, rotation: Double?) {
@@ -126,7 +146,8 @@ public final class CoreGraphicsContext: GraphicsContext {
         let attributedString = NSAttributedString(string: text.value, attributes: attributes)
         let line = CTLineCreateWithAttributedString(attributedString)
         let bounds = CTLineGetImageBounds(line, cgContext)
-        cgContext.textPosition = CGPoint(x: bounds.minX, y: bounds.minY)
+        let offset = cgPoint(for: text.position)
+        cgContext.textPosition = CGPoint(x: bounds.minX + offset.x, y: bounds.minY + offset.y)
         CTLineDraw(line, cgContext)
     }
 }
@@ -139,32 +160,6 @@ extension Color {
             blue: CGFloat(blue) / 255.0,
             alpha: CGFloat(alpha) / 255.0
         )
-    }
-}
-
-extension CGPoint {
-    init(_ vec2: Vec2<Double>) {
-        self.init(x: vec2.x, y: vec2.y)
-    }
-}
-
-extension CGSize {
-    init(_ vec2: Vec2<Double>) {
-        self.init(width: vec2.x, height: vec2.y)
-    }
-
-    init(_ vec2: Vec2<Int>) {
-        self.init(width: vec2.x, height: vec2.y)
-    }
-}
-
-extension CGRect {
-    init(_ rect: Rectangle<Double>) {
-        self.init(x: rect.topLeft.x, y: rect.topLeft.y, width: rect.width, height: rect.height)
-    }
-
-    init(_ ellipse: Ellipse<Double>) {
-        self.init(ellipse.boundingRectangle)
     }
 }
 #endif
