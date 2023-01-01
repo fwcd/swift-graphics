bc5c53f71d1cb44a6815ae33556d57e0c924b381
fwcd <fwcdmail@gmail.com>
2023-01-01 21:39:46 +0100

    Reimplement horizontallyImageJoined in terms of DefaultGraphicsContext

--- Sources/Graphics/image/CollectionImageUtils.swift
+++ Sources/Graphics/image/CollectionImageUtils.swift
@@ -1,13 +1,13 @@
 import Utils
 
-extension Array where Element == CairoContext.Image {
+extension Array where Element == DefaultGraphicsContext.Image {
     public func horizontallyImageJoined() -> Element? {
         guard !isEmpty else { return nil }
 
         let totalWidth = map { $0.width }.reduce(0, +)
         let totalHeight = map { $0.height }.max() ?? 0
 
-        guard let ctx = try? CairoContext(width: totalWidth, height: totalHeight) else { return nil }
+        guard let ctx = try? DefaultGraphicsContext(width: totalWidth, height: totalHeight) else { return nil }
         var pos = Vec2<Double>(x: 0.0, y: 0.0)
 
         for image in self {
@@ -15,6 +15,6 @@ extension Array where Element == CairoContext.Image {
             pos = pos + Vec2(x: Double(image.width), y: 0.0)
         }
 
-        return ctx.makeImage()
+        return try? ctx.makeImage()
     }
 }
