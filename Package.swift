d57c3f5b4deb36009301ae3628bb2400f466c8e4
fwcd <fwcdmail@gmail.com>
2023-02-20 02:01:01 +0100

    Bump swift-utils to 2.0.0

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
-        .package(url: "https://github.com/fwcd/swift-utils.git", from: "1.1.0"),
+        .package(url: "https://github.com/fwcd/swift-utils.git", from: "2.0.0"),
         .package(url: "https://github.com/fwcd/swift-cairo.git", from: "1.3.3"),
     ],
     targets: [
