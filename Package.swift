66730e557302a97d75a6b242e27d630eda5d4e73
fwcd <fwcd@fwcd.dev>
2024-02-13 02:19:22 +0100

    Bump swift-utils to 3.0.0 and macOS requirement to 13

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "swift-graphics",
+    platforms: [.macOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "Graphics", targets: ["Graphics"]),
@@ -15,7 +16,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
-        .package(url: "https://github.com/fwcd/swift-utils.git", from: "2.0.0"),
+        .package(url: "https://github.com/fwcd/swift-utils.git", from: "3.0.0"),
         .package(url: "https://github.com/fwcd/swift-cairo.git", from: "1.3.3"),
     ],
     targets: [
