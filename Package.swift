4106061133bca2f38b398e5ce207e8d6b73adb43
fwcd <fwcd@fwcd.dev>
2024-02-13 18:46:55 +0100

    Bump swift-cairo to 1.3.4

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
         .package(url: "https://github.com/fwcd/swift-utils.git", from: "3.0.0"),
-        .package(url: "https://github.com/fwcd/swift-cairo.git", from: "1.3.3"),
+        .package(url: "https://github.com/fwcd/swift-cairo.git", from: "1.3.4"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
