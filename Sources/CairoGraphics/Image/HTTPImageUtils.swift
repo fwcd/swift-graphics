1b9fe64163977170d46fc22a418fe56e26cc9c10
fwcd <fwcd@fwcd.dev>
2024-07-03 22:35:04 +0200

    Bump swift-utils and add async HTTPRequest.fetchPNG extension

--- Sources/CairoGraphics/Image/HTTPImageUtils.swift
+++ Sources/CairoGraphics/Image/HTTPImageUtils.swift
@@ -4,4 +4,8 @@ extension HTTPRequest {
     public func fetchPNGAsync() -> Promise<CairoImage, Error> {
         runAsync().mapCatching { try CairoImage(pngData: $0) }
     }
+
+    public func fetchPNG() async throws -> CairoImage {
+        try await CairoImage(pngData: run())
+    }
 }
